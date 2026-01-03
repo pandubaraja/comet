843eac964e48edc0e7003bf20b4024943896b1b5
pandubaraja <pandu.baraja@gmail.com>
2026-01-03 09:35:49 +0700

    Set jvmTarget to JVM 11

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -32,7 +32,11 @@ kotlin {
             }
         }
     }
-    jvm()
+    jvm {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
     iosX64()
     iosArm64()
     iosSimulatorArm64()
